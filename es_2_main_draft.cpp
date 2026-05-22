4c83f52c00d986b263458330ea9e9bd8ed5f88c4
mattiavincentelli <mattiavincentelli5@gmail.com>
2026-05-22 19:30:19 +0200

    testato e modificate alcune funzioni

--- es_2_main_draft.cpp
+++ es_2_main_draft.cpp
@@ -13,8 +13,8 @@ using namespace std;
 #define MAX_shapeS 10
 
 void Print_All_Pol(shape *shape[], int n);
-void Modify_Pol(shape *shape[], grid *grid);
-void Change_Pos(shape *shape[], grid *grid);
+void Modify_Pol(shape *shape[], grid *grid, int n);
+void Change_Pos(shape *shape[], grid *grid, int n);
 void Insert_New_Pol(shape *shape[], int *n, grid *grid);
 void Delete_Pol(shape *shape[], int n);
 void Delete_All_Pols(shape *shape[], int n);
@@ -30,12 +30,11 @@ int main()
     while(running) {
         int choice;
         cout << "MENU: " << endl << "1 - Visualizza tutti i poligoni" << endl;
-        cout << "2 - Modifica le proprietà di un poligono" << endl << "3 - Sposta un poligono sulla griglia";
-        cout << "4 - Inserisci un nuovo poligono" << endl << "5 - Cancella un poligono";
-        cout << "6 - Cancella tutti i poligono" << endl << "0 - Esci";
+        cout << "2 - Modifica le proprietà di un poligono" << endl << "3 - Sposta un poligono sulla griglia" << endl;
+        cout << "4 - Inserisci un nuovo poligono" << endl << "5 - Cancella un poligono" << endl;
+        cout << "6 - Cancella tutti i poligoni" << endl << "0 - Esci" << endl;
         
-        if(!cin >> choice)
-            cout << "inserire uno dei numeri interi indicati" << endl;
+        cin >> choice;
         
         if(choice < 0 || choice > 6)
             cout << "scelta non disponibile" << endl;
@@ -45,22 +44,22 @@ int main()
                 running = 0;
                 break;
             case 1:
-                Print_All_Pol(&shapes[nshapes], nshapes);
+                Print_All_Pol(shapes, nshapes);
                 break;
             case 2:
-                Modify_Pol(&shapes[nshapes], my_grid);
+                Modify_Pol(shapes, my_grid, nshapes);
                 break;
             case 3:
-                Change_Pos(&shapes[nshapes], my_grid);
+                Change_Pos(shapes, my_grid, nshapes);
                 break;
             case 4:
-                Insert_New_Pol(&shapes[nshapes], nshapes_ptr, my_grid);
+                Insert_New_Pol(shapes, nshapes_ptr, my_grid);
                 break;
             case 5:
-                Delete_Pol(&shapes[nshapes], nshapes);
+                Delete_Pol(shapes, nshapes);
                 break;
             case 6:
-                Delete_All_Pols(&shapes[nshapes], nshapes);
+                Delete_All_Pols(shapes, nshapes);
                 break;
             default: 
                 break;
@@ -73,27 +72,41 @@ int main()
     for(int i = 0; i < nshapes; i++)
         delete shapes[i];
 
+    delete my_grid;
+
     return 0;
 }
 
 
 
 void Print_All_Pol(shape *shape[], int n) {
+    if(!shape) 
+        cout << "Nessuno poligono ancora creato" << endl;
+        
     for (int i = 0; i < n; i++) {
         cout << endl << "Figura [" << i << "]" << endl;
         shape[i]->Dump();
     }
 }
 
-void Modify_Pol(shape *shape[], grid *grid) {
+void Modify_Pol(shape *shape[], grid *grid, int n) {
     float new_w, new_h;
     string new_text;
-    const char* text_ptr = new_text.c_str();
     int n_Pol;
 
-    
+    for(int i = 0; i < n; i++) {
+        cout << "poligoni che puoi modificare" << i << endl;
+        shape[i]->Dump();
+    }
+
     cout << "quale poligono vuoi modificare?";
     cin >> n_Pol;
+
+    if(n_Pol < 0 || n_Pol > n - 1) {
+        cout << "Poligono non valido" << endl;
+        exit(0);
+    }
+  
 
     cout << "Come vuoi modificare l'ampiezza?" << endl; 
     cin >> new_w;
@@ -103,7 +116,11 @@ void Modify_Pol(shape *shape[], grid *grid) {
     cin >> new_h;
 
     cout << "Come vuoi modificare il testo?" << endl; 
+    cin.ignore();
+    
     getline(cin, new_text);
+    const char* text_ptr = new_text.c_str();
+
 
     if(shape[n_Pol]->GetWidth() + new_w > grid->GetWidth() || shape[n_Pol]->GetHeight() + new_h > grid->GetHeight())
         cout << "poligono fuori dalla griglia" ; 
@@ -115,28 +132,37 @@ void Modify_Pol(shape *shape[], grid *grid) {
     
 }
 
-void Change_Pos(shape *shape[], grid *grid) {
-    float new_w, new_h;
+void Change_Pos(shape *shape[], grid *grid, int n) {
+    float new_x, new_y;
     
     int n_Pol;
 
-    
+    for(int i = 0; i < n; i++) {
+        cout << "poligoni di cui puoi cambiare la posizione" << i << endl;
+        shape[i]->Dump();
+    }
+
+
     cout << "quale poligono vuoi modificare?";
     cin >> n_Pol;
 
-    cout << "Come vuoi modificare l'ampiezza?" << endl; 
-    cin >> new_w;
+      if(n_Pol < 0 || n_Pol > n - 1 ) {
+        cout << "Poligono non valido" << endl;
+        exit(0);
+    }
+
+    cout << "Come vuoi modificare la x?" << endl; 
+    cin >> new_x;
 
 
-    cout << "Come vuoi modificare l'altezza?" << endl; 
-    cin >> new_h;
+    cout << "Come vuoi modificare la y?" << endl; 
+    cin >> new_y;
 
 
-    if(shape[n_Pol]->GetWidth() + new_w > grid->GetWidth() || shape[n_Pol]->GetHeight() + new_h > grid->GetHeight())
+    if(shape[n_Pol]->GetWidth() + new_x > grid->GetWidth() || shape[n_Pol]->GetHeight() + new_y > grid->GetHeight())
         cout << "poligono fuori dalla griglia" ; 
     else{
-        shape[n_Pol]->SetWidth(new_w);
-        shape[n_Pol]->SetHeight(new_h);
+        shape[n_Pol]->SetPosition(new_x, new_y);
     }
 }
 
@@ -163,9 +189,11 @@ void Insert_New_Pol(shape *shape[], int *n, grid *grid) {
     cin >> width;
 
 
-    if((choice) < 4  || choice > 0 || x + width < grid->GetWidth() || y + height < grid->GetHeight()) {
+    if((choice) > 3  || choice < 0 || x + width > grid->GetWidth() || y + height > grid->GetHeight()) {
+       cout << "Specifiche non valide" << endl;
+    }else {
         switch(choice) {
-            case 1:
+            case 1: 
                 shape[*n] = new rectangle(x, y, width, height); 
                 break;
             case 2:
@@ -177,23 +205,32 @@ void Insert_New_Pol(shape *shape[], int *n, grid *grid) {
             default:
                 break;
         }
-    }else
-        cout << "Specifiche non valide" << endl;
-    
-    
+    }
+         
+    (*n)++;
 }
 
 
 void Delete_Pol(shape *shape[], int n) {
     int choice;
 
+     for(int i = 0; i < n; i++) {
+        cout << "poligoni che puoi cancellare" << i << endl;
+        shape[i]->Dump();
+    }
+
     cout << "Quale poligono vuoi cancellare?" << endl; 
     cin >> choice;
 
-    if(choice < 0 || choice > n)
+    if(choice < 0 || choice >= n)
         cout << "Scelta non valida" << endl; 
     
     delete shape[choice];
+
+    for(int i = choice; i < n - 1; i++)
+        shape[i] = shape[i + 1];
+
+    shape[n - 1] = nullptr;
 }
 
 void Delete_All_Pols(shape *shape[], int n) {
